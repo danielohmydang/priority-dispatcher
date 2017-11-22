a30a7b1c8c37ddd42f5bacbf841671e4449832b5
EC2 Default User <ddang003@odu.edu>
2017-11-22 22:10:27 +0000

    Created initial processes

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,39 +1,68 @@
 
-var isEditing = false,
-    tempNameValue = "",
-    tempDateValue = "";
-
 // create new process list
 var processList = [];
 var processCounter = 0;
 
-function main() {
-    // Create 5 random inital processes
-    for (var i = 0; i < 5; i++) {
-        processList.push({
-            pid: i,
-            status: "ready",
-            priority: Math.floor(Math.random() * 5)
-        })
-        processCounter++;
+// Create 5 random inital processes
+for (var i = 0; i < 5; i++) {
+    processList.push({
+        pid: i,
+        status: "ready",
+        priority: Math.floor(Math.random() * 10),
+        state: "running",
+        delete: "DELETE"
+    })
+    processCounter++;
+}
+// sort the array by priority 
+processList.sort(function (a, b) {
+    return a.priority - b.priority;
+});
+
+function buildTable(labels, objects, container) {
+  var table = document.createElement('table');
+  var thead = document.createElement('thead');
+  var tbody = document.createElement('tbody');
+
+  var theadTr = document.createElement('tr');
+  for (var i = 0; i < labels.length; i++) {
+    var theadTh = document.createElement('th');
+    theadTh.innerHTML = labels[i];
+    theadTr.appendChild(theadTh);
+  }
+  thead.appendChild(theadTr);
+  table.appendChild(thead);
+
+  for (j = 0; j < objects.length; j++) {
+    var tbodyTr = document.createElement('tr');
+    for (k = 0; k < labels.length; k++) {
+      var tbodyTd = document.createElement('td');
+      tbodyTd.innerHTML = objects[j][labels[k].toLowerCase()];
+      tbodyTr.appendChild(tbodyTd);
     }
+    tbody.appendChild(tbodyTr);
+  }
+  table.appendChild(tbody);
+
+  container.appendChild(table);
+}
+
+
+function main() {
     // sort the array by priority 
     processList.sort(function (a, b) {
         return a.priority - b.priority;
     });
 
-    console.log(processList);
-
-    isEditing = true;
+    var Headerlabels = ['PID', 'Status', 'Priority', 'State', 'Delete']; 
+    buildTable(Headerlabels, processList, document.getElementById("content-table"));
+ 
 }
 
 main();
 
 $('.new-row').on('click', function() {
-    var tableBody = $(this).closest('tbody'),
-    trNew = '<tr><td class="pid"><input type="text" name="name" value=""></td><td class="status"><input type="text" name="status" value=""></td><td class="priority"><input type="text" name="priority" value=""></td><td class="state"><input type="text" name="state" value=""></td><td class="trash"><i class="fa fa-ban" aria-hidden="true"></i></td></tr>';
-
-    if (isEditing) {
-        var pidInput = tableBodyh
-    }
+    console.log('hello');
 });
+
+
